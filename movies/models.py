5df6d0c3c95c1da41a4b5df6066bb27c111ebce0
Miles Budden <miles@budden.net>
2018-10-29 20:45:56 +0000

    Added a rental as a db table

--- movies/models.py
+++ movies/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Movie(models.Model):
     movie_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    movie_logo = models.FileField()
+    movie_logo = models.ImageField()
     liked = models.BooleanField(default=False)
     description = models.TextField(blank=True, null=True)
     release_date = models.DateField()
@@ -14,6 +14,21 @@ class Movie(models.Model):
     def __str__(self):
         return self.movie_title + '-' + self.genre
 
+
+class Rental(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE
+    )
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return self.owner.get_full_name() + ": " + self.movie.movie_title 
+        
 
 
 
