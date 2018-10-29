eeb0a0cbfcf28eb82478eec09ea3a6ffed9d110f
Miles Budden <miles@budden.net>
2018-10-29 21:11:14 +0000

    Made a start with materialize

--- movies/models.py
+++ movies/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Movie(models.Model):
     movie_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    movie_logo = models.ImageField()
+    movie_logo = models.FileField()
     liked = models.BooleanField(default=False)
     description = models.TextField(blank=True, null=True)
     release_date = models.DateField()
