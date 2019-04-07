3456cfe7cb585a6c53cf2b7bc33a15fbe9f3460c
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2019-04-07 16:23:33 +0100

    added adminplus to notflix

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Movie, Genre
+from django.core import management
+from django.shortcuts import redirect
 
 # Add the movie model to the admin DB
 
@@ -7,4 +9,21 @@ class GenreAdmin(admin.ModelAdmin):
     list_display = ['genre', 'slug']
     prepopulated_fields = {'slug': ('genre',)}
 admin.site.register(Genre, GenreAdmin)
-admin.site.register(Movie)
+
+class MovieAdmin(admin.ModelAdmin):
+    @admin.site.register_view('import_movies_from_json_file', 'Import Movies from Json')
+    def import_movies_from_url(request):
+        print('import movies here')
+        try:
+            management.call_command('import_from_json_file')
+            message = 'successfully imported data from Json file'
+
+        except Exception as ex:
+            message = 'Error importing from data from JSON file {}'.format(str(ex))
+
+        admin.ModelAdmin.message_user(Movie, request, message)
+        return redirect('admin:index')
+
+admin.site.register(Movie, MovieAdmin)
+
+# admin.site.register(Movie)
--- notflix/urls.py
+++ notflix/urls.py
@@ -5,6 +5,12 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
+from adminplus.sites import AdminSitePlus
+
+# Admin setup
+admin.site = AdminSitePlus()
+admin.autodiscover()
 
 
 urlpatterns = [
