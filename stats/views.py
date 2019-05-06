77abef2ea676bfbb8948d1e7936eaaa116a18c6d
Miles Budden <miles@budden.net>
2019-05-06 13:11:18 +0100

    Added a stats page

--- stats/views.py
+++ stats/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotAllowed, HttpResponse
+from movies.models import Movie, Like, Dislike
 
 
 # Create your views here.
 def main(request):
     if request.user.is_superuser:
-        return render(request, 'stats/main.html')
+        context = {}
+        context['number'] = Movie.objects.all().count()
+        context['likes'] = Like.objects.all().count()
+        context['dislikes'] = Dislike.objects.all().count()
+
+        return render(request, 'stats/main.html', context=context)
     else:
         return HttpResponseNotAllowed("Hello there")
 
